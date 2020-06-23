356324ddf4f132c124acc9c52f34dbb6280c0107
andreneves <andr@andr.com.br>
2020-06-23 20:18:12 -0300

    up

--- BDIIA05.sql
+++ BDIIA05.sql
@@ -20,3 +20,184 @@ USE bd2;
 -- INDEX - Usado para criar e recuperar dados do banco de dados muito rapidamente
 
 
+-- OBS:
+-- Para verficar as modificações na tabela do banco de dados
+SHOW COLUMNS FROM alunos;
+
+
+-- NOT NULL
+-- Não aceita valores null
+-- atua tanto na inserção como na atualização de dados
+
+-- criação
+CREATE TABLE alunos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(80) NOT NULL
+); 
+
+
+insert into alunos (nome) values (null);
+
+-- alteração
+
+CREATE TABLE funcionarios (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(80)
+); 
+
+ALTER TABLE funcionarios
+MODIFY nome varchar(80) NOT NULL; 
+
+insert into funcionarios (nome) values (null);
+
+---------------------------------------------------------------------------------------------------------------
+
+-- UNIQUE
+-- Valores precisam ser unicos
+-- chaves primárias são obrigatoriamente unicas
+
+-- criação
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    UNIQUE (email)
+);
+
+insert into usuarios (nome, email) values ('André', 'andr@andr.com.br');
+insert into usuarios (nome, email) values ('André 2', 'andr@andr.com.br');
+
+-- na alteração
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+ALTER TABLE usuarios
+ADD UNIQUE (email); 
+
+-- OBS.: No caso de multiplas colunas
+-- ALTER TABLE tabela
+-- ADD CONSTRAINT nome_da_constraint UNIQUE (campo1, campo2); 
+
+-- Exclusão de unique
+-- ALTER TABLE tabela
+-- DROP INDEX nome_da_constraint; 
+
+-- -------------------------------------------------------------------------------------------------------------
+
+-- PRIMARY KEY
+-- unicos e não nulos
+-- tem que ser unicos, mas podem ser constituidos e uma ou mais colunas
+
+-- criação
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+-- ou 
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    PRIMARY KEY (id)    
+);
+
+-- Multiplas colunas - chave primária composta
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+CONSTRAINT PRIMARY KEY (id, nome)
+);
+
+-- SHOW COLUMNS FROM usuarios;
+
+-- na alteração
+CREATE TABLE usuarios (
+    id INT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+-- unica
+ALTER TABLE usuarios
+ADD PRIMARY KEY (id); 
+
+-- composta
+ALTER TABLE usuarios
+ADD CONSTRAINT PRIMARY KEY (id, nome); 
+
+-- EXCLUSAO
+ALTER TABLE usuarios
+DROP PRIMARY KEY; 
+
+-- -------------------------------------------------------------------------------------------------------------
+
+-- FOREIGN KEY - chave estrangeira
+-- É uma chave utilizada para estabelecer relação entre duas tabelas
+-- É um, ou mais campos, de uma tabela que se referem a chave primária de outra tabela.
+-- A tabela que contém a chave é chamada de tabela filho e a tabela que contém a chave candidata é chamada de tabela pai.
+
+-- create table
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `valor` float NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
+);
+
+-- na alteração
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `valor` float NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE produtos
+ADD FOREIGN KEY (categoria_id) REFERENCES categorias(id); 
+
+-- insert categorias 
+insert into categorias (nome) values ('eletronicos');
+insert into categorias (nome) values ('informatica');
+insert into categorias (nome) values ('escritorio');
+
+-- insert produtos 
+insert into produtos (nome, valor, categoria_id) values ('microondas', 100, 1);
+insert into produtos (nome, valor, categoria_id) values ('forno eletrico', 200, 1);
+insert into produtos (nome, valor, categoria_id) values ('teclado', 50, 2);
+insert into produtos (nome, valor, categoria_id) values ('mouse', 20, 2);
+insert into produtos (nome, valor, categoria_id) values ('mesa', 120, 3);
+insert into produtos (nome, valor, categoria_id) values ('cadeira', 80, 3);
+
+-- selecionar todos os dados da tabela categorias
+select * from categorias;
+
+-- selecionar todos os dados da tabela produtos
+select * from produtos;
+
+-- inner join das duas tabelas
+SELECT categorias.nome, produtos.nome, produtos.valor
+FROM produtos
+INNER JOIN categorias ON produtos.categoria_id = categorias.id;
+
+-- excluindo chave estrangeira
+-- ALTER TABLE produtos
+-- DROP FOREIGN KEY nome_da_chave; 
+
+
