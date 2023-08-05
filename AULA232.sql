d2f2c4baf21dd48e06f40e347f8dade733564c1e
andreneves <andr@andr.com.br>
2023-08-04 21:54:16 -0300

    up

--- AULA232.sql
+++ AULA232.sql
@@ -5,3 +5,13 @@
 comentario
 duas linhas
 */
+
+-- criacao de banco de dados
+create database bd2;
+
+use bd2;
+
+create table produtos (
+	id int auto_increment primary key,
+    nome varchar(255)
+);
