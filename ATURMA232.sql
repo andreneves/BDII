005da2cbcdb2e8147b37366f846b1bdf194aca2c
andreneves <andr@andr.com.br>
2023-09-01 21:54:38 -0300

    up

--- ATURMA232.sql
+++ ATURMA232.sql
@@ -58,3 +58,82 @@ set
 nome = 'Esponja de aço',
 quantidade = 40
 where id = 3;
+
+-- deletando registros
+delete from produtos
+where id = 3;
+
+-- Renomar uma tabela
+
+create table estudantes (
+	id int(11) primary key auto_increment,
+    nome varchar(255) not null
+);
+
+rename table estudantes to alunos;
+
+
+-- Destroi uma tabela
+drop table estudantes;
+
+-- TRUNCATE - exclui todas as informações da tabela e volta o auto_incremente para 1
+select * from produtos;
+
+delete from produtos where id >= 1;
+-- VS
+truncate table produtos;
+
+-- para saber o valor do auto_increment
+SELECT AUTO_INCREMENT FROM information_schema.tables
+WHERE table_name = 'produtos' AND table_schema = 'bd2';
+
+-- -------------
+-- ALTER TABLE
+-- -------------
+
+-- Alterar a estrutura de uma tabela - Acrescentando, modificando ou excluindo colunas
+
+create table servicos (
+	id int(11) primary key auto_increment,
+    nome varchar(255) not null
+);
+
+
+-- ADD - adicionar coluna
+alter table servicos
+add descricao text;
+
+alter table servicos
+add preco float(8,2);
+
+-- inserido de propósito errado
+alter table servicos
+add cor varchar(255);
+
+-- DROP - excluir uma coluna
+alter table servicos
+drop cor;
+
+-- MODIFY - modificar coluna de uma tabela
+alter table servicos
+modify column descricao varchar(255);
+
+show columns from servicos;
+
+-- simulando um erro de modificacao de coluna - INICIO
+
+insert into servicos (nome, descricao, preco)
+values ('Pintura', 'Uma pintura muito bem feita e com ótimo resultado', 500);
+
+alter table servicos
+modify column descricao varchar(20);
+
+alter table servicos
+modify column preco int(11);
+
+insert into servicos (nome, descricao, preco)
+values ('Reforma', 'Uma reforma', 150.45);
+
+-- simulando um erro de modificacao de coluna - FIM
+-- teste
+select * from servicos;
