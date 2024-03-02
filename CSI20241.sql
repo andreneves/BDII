b9d1710b72da147a6dcd47a00af1703aec7bb434
andreneves <andr@andr.com.br>
2024-03-01 21:07:15 -0300

    atualizacao aula 1

---
+++ CSI20241.sql
@@ -0,0 +1,20 @@
+-- Comentário de uma linha
+
+/*
+Comentário de
+mais de uma
+linha
+*/
+
+-- criação de um banco de dados
+CREATE DATABASE bd2;
+
+-- excluir um banco de dados
+DROP DATABASE bd2;
+
+-- seleciona um banco de dados
+USE bd2;
+
+
+
+
